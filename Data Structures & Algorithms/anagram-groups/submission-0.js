27380e4471fd8c0b54bf83a6213b4788466c350c
Haongg <153846526+Haongg@users.noreply.github.com>
2026-08-15 10:06:44 +0700

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = [];
+        const temp = [...strs];
+        const resMap = new Map();
+
+        for (let i=0 ; i<temp.length ; i++){
+            temp[i] = temp[i].split('').sort().join('');
+            if (!resMap.has(temp[i])){
+                resMap.set(temp[i],[strs[i]]);
+            } else resMap.get(temp[i]).push(strs[i]);
+        }
+
+        for (const [key, value] of resMap.entries()){
+            res.push(value);
+        }
+        
+        return res;
+    }
+}
